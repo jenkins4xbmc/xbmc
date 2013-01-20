05843318a9b9963064a61a3468d7813e7bf208cf
wsoltys <wiso@no.way>
2013-01-20 19:37:14 +0100

    delay load FreeImage.dll

--- xbmc/win32/Win32DelayedDllLoad.cpp
+++ xbmc/win32/Win32DelayedDllLoad.cpp
@@ -59,6 +59,12 @@ FARPROC WINAPI delayHookNotifyFunc (unsigned dliNotify, PDelayLoadInfo pdli)
         HMODULE hMod = LoadLibraryEx(strDll.c_str(), 0, LOAD_WITH_ALTERED_SEARCH_PATH);
         return (FARPROC)hMod;
       }
+      if (stricmp(pdli->szDll, "FreeImage.dll") == 0)
+      {
+        CStdString strDll = CSpecialProtocol::TranslatePath("special://xbmcbin/system/FreeImage.dll");
+        HMODULE hMod = LoadLibraryEx(strDll.c_str(), 0, LOAD_WITH_ALTERED_SEARCH_PATH);
+        return (FARPROC)hMod;
+      }
       break;
   }
   return NULL;
